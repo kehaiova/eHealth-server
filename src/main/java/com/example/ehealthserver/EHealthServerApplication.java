19bcdca17ff98b1ffd2f57de8981daa93a0d8742
kehaiova <kehaiovaaaa@gmail.com>
2022-10-02 01:15:21 +0300

    start

---
+++ src/main/java/com/example/ehealthserver/EHealthServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ehealthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EHealthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EHealthServerApplication.class, args);
+    }
+
+}
